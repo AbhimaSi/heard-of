1deec46cf5df206b7594c89e533639efe9fe6cc8
Abhimael Sidney Setni Rogatti <abhima_si@outlook.com>
2025-11-30 04:00:43 -0300

    Added mongodb config

---
+++ backend/src/config/mongodb.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://localhost:27017/db';
+
+let client = null;
+let db = null;
+
+const getDB = async () => {
+    try{
+        if (client && db)
+            return db;
+
+        client = new MongoClient(url);
+        await client.connect('db');
+        db = client.db('db');
+        console.log('Connected to DB'); //DEBUG
+        return db;
+    }
+
+    catch(err){
+        throw err;
+    }
+}
+
+module.exports = getDB;
