1356670d0bbb75832509e7f7df3fb8b02a3c2a66
Abhimael Sidney Setni Rogatti <abhima_si@outlook.com>
2025-11-30 04:03:18 -0300

    Added user Scheme/Model

---
+++ backend/src/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pass: { type: String, required: true }
+})
+
+const UserModel = mongoose.model('User', userSchema)
+
+module.exports = UserModel
